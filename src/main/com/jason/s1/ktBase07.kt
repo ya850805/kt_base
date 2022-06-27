30a6cb32ee0e4024d891e9060a4d8a8627b46000
Jason <ya850805@gmail.com>
2022-06-28 01:51:19 +0800

    feat: Add Kotlin primitive comments.

--- src/main/com/jason/s1/ktBase07.kt
+++ src/main/com/jason/s1/ktBase07.kt
@@ -4,17 +4,34 @@ package com.jason.s1
 fun main() {
     println("Hello world")
 
+    /***************聲明變數***************/
+
     /**
-     * 聲明變數
      * var: 可讀可改
      * name: 變數名
      * String: 類型
      * "Jason": 值
-     *
      */
     var name: String = "Jason"
 //    name = "Jason1"
     println(name)
 
-    
+    /***************內置數據類型***************/
+
+    /**
+     * 以下都是kotlin的類，不是Java的
+     * String: 字符串
+     * Char: 單字符
+     * Boolean: true/false
+     * Int: 整數
+     * Double: 小數
+     * List: 集合
+     * Set: 無重複的元素集合
+     * Map: 鍵值對的集合
+     *
+     *=======================================
+     *
+     * Int會經過編譯轉換為Java的int
+     * Float會經過編譯轉換為Java的float
+     */
 }
