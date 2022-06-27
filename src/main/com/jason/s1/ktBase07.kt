6b14306fc94d3de6c8d4e414dcde343087bd1532
Jason <ya850805@gmail.com>
2022-06-28 01:41:26 +0800

    Initial commit

---
+++ src/main/com/jason/s1/ktBase07.kt
@@ -0,0 +1,20 @@
+package com.jason.s1
+
+//Kotlin語言聲明變數與內置數據類型
+fun main() {
+    println("Hello world")
+
+    /**
+     * 聲明變數
+     * var: 可讀可改
+     * name: 變數名
+     * String: 類型
+     * "Jason": 值
+     *
+     */
+    var name: String = "Jason"
+//    name = "Jason1"
+    println(name)
+
+    
+}
