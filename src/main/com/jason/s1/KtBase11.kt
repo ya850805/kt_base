3b4a3af22ed8311774f65d4c710b551868854dd2
Jason <ya850805@gmail.com>
2022-06-28 22:49:08 +0800

    feat: Add show Kotlin byte code demo.

---
+++ src/main/com/jason/s1/KtBase11.kt
@@ -0,0 +1,15 @@
+package com.jason.s1
+
+const val PI1 = 3.1415
+const val PI2 = 3.1415
+const val AGE = 99
+
+//查看Kotlin反編譯後字節碼
+fun main() {
+    val name = "Jason"
+    val sex = 'M'
+    println(sex)
+
+    val number = 108
+    println(number)
+}
