59e78f865e8043b1158f32b524e5cbfd257c531d
Jason <ya850805@gmail.com>
2022-06-30 11:28:31 +0800

    feat: Add exception handle and custom exception demo.

---
+++ src/main/com/jason/s2/KtBase41.kt
@@ -0,0 +1,20 @@
+package com.jason.s2
+
+//Kotlin語法中異常處理與自定義異常特點
+fun main() {
+    try {
+        var info: String? = null
+        
+        checkException(info)
+        
+        println(info!!.length)
+    } catch (e: Exception) {
+        println("錯誤：$e")
+    }
+}
+
+fun checkException(info: String?) {
+    info ?: throw CustomException()
+}
+
+class CustomException: RuntimeException("代碼異常")
