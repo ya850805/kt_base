3d455e601f4a1a077bec1e98480c6b67e9508202
Jason <ya850805@gmail.com>
2022-07-04 23:26:38 +0800

    feat: Add generic class demo.

---
+++ src/main/com/jason/s6/KtBase103.kt
@@ -0,0 +1,27 @@
+package com.jason.s6
+
+//Kotlin語言的定義範型類
+fun main() {
+    val student1 = Student0("Jason", 25, 'M')
+    val student2 = Student0("Juan", 26, 'F')
+
+    val teacher1 = Teacher0("Tom", 40, 'M')
+    val teacher2 = Teacher0("Marry", 36, 'F')
+
+    KtBase103(student1).show()
+    KtBase103(student2).show()
+    KtBase103(teacher1).show()
+    KtBase103(teacher2).show()
+
+    KtBase103(String("Hi".toByteArray())).show()
+}
+
+/**
+ * 物件輸出器
+ */
+class KtBase103<T>(private val obj: T) {
+    fun show() = println("輸出:$obj")
+}
+
+data class Student0(val name: String, val age: Int, val sex: Char)
+data class Teacher0(val name: String, val age: Int, val sex: Char)
