af4a28b4a77b1072e9b956ad89720f9b65502528
Jason <ya850805@gmail.com>
2022-07-05 21:42:55 +0800

    feat: Add in/out keyword demo.

---
+++ src/main/com/jason/s6/KtBase111.kt
@@ -0,0 +1,39 @@
+package com.jason.s6
+
+//Kotlin語言中使用in和out
+fun main() {
+    /**
+     * 逆變 in T
+     */
+    SetClass<String>().set1("item!!!")
+
+    /**
+     * 協變 out T
+     */
+    println(GetClass<String>("Jason").get1())
+}
+
+/**
+ * 整個SetClass中，範型T相關的只能當作輸入，不能當作輸出 -> 逆變
+ */
+class SetClass<in T>() {
+    fun set1(item: T) = println("set1 設置的item是$item")
+
+    /**
+     * 編譯不通過，T不能給外界讀取
+     */
+//    fun get1(): T? = null
+}
+
+/**
+ * 整個GetClass中，範型T相關的只能當作輸出，不能當作輸入 -> 協變
+ */
+class GetClass<out T>(private val item: T) {
+
+    /**
+     * 編譯不通過，T只能讀取，不能給外界輸入(修改)
+     */
+//    fun set1(item: T) = println("set1 設置的item是$item")
+
+    fun get1(): T = item
+}
