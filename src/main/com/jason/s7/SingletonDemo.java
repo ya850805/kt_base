d04cdd931a4a3bf0a3f34f518a2d787d1f5b7931
Jason <ya850805@gmail.com>
2022-07-11 02:32:36 +0800

    feat: Add HungrySingleton demo with Java & Kotlin.

---
+++ src/main/com/jason/s7/SingletonDemo.java
@@ -0,0 +1,14 @@
+package com.jason.s7;
+
+/**
+ * 餓漢式的實現，Java版本
+ */
+public class SingletonDemo {
+    private static SingletonDemo INSTANCE = new SingletonDemo();
+
+    private SingletonDemo() {}
+
+    public static SingletonDemo getInstance() {
+        return INSTANCE;
+    }
+}
---
+++ src/main/com/jason/s7/SingletonDemoKt.kt
@@ -0,0 +1,6 @@
+package com.jason.s7
+
+/**
+ * 餓漢式的實現，Kotlin版本
+ */
+object SingletonDemoKt
