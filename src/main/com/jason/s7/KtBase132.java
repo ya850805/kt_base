8e43afd784db0d5ce242268777dbf80ab3074952
Jason <ya850805@gmail.com>
2022-07-12 02:26:02 +0800

    feat: Add @JvmField demo.

---
+++ src/main/com/jason/s7/KtBase132.java
@@ -0,0 +1,8 @@
+package com.jason.s7;
+
+public class KtBase132 {
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println(p.names);
+    }
+}
---
+++ src/main/com/jason/s7/KtBase132.kt
@@ -0,0 +1,7 @@
+package com.jason.s7
+
+//註解@JvmField與Kotlin
+class Person {
+    @JvmField
+    val names = listOf("Jason", "Juan", "Tom", "Marry")
+}
