df2867e56451ba8c936cec734d3581f2ae14d418
Jason <ya850805@gmail.com>
2022-07-12 01:53:24 +0800

    feat: Add @file:JvmName demo.

---
+++ src/main/com/jason/s7/KtBase131.java
@@ -0,0 +1,7 @@
+package com.jason.s7;
+
+public class KtBase131 {
+    public static void main(String[] args) {
+        Stu.getStudentName("Jason");
+    }
+}
---
+++ src/main/com/jason/s7/KtBase131.kt
@@ -0,0 +1,10 @@
+@file:JvmName("Stu")
+
+package com.jason.s7
+
+//註解@JvmName與Kotlin
+fun main() {
+
+}
+
+fun getStudentName(str: String) = println(str)
